fd5707c4f71980767e2173898b1aa7a769d98017
ke260g <ke260g@gmail.com>
2020-08-14 00:09:17 +0800

    “daily update"

--- pro-shell/NoteTask/flac_cue.sh
+++ pro-shell/NoteTask/flac_cue.sh
@@ -1,6 +1,14 @@
-apt install wavpack cuetools flac mac shntool
+apt install wavpack cuetools flac mac shntool abcde mkcue
 
+# step_1: extract flac from a cd
+abcde -1 -o flac
 
-# split flac/wav from cue
-shntool split -t "%p" -o flac -d out_dir -f input.cue input.flac
+# step_2: create a cue-template
+mkcue > input.cue
+
+# step_3: edit input.cue
+nano input.cue
+
+# step_4: split flac/wav from cue
+shntool split -t "%t.%p" -o flac -d out_dir -f input.cue input.flac
 ### -t "%p" format the filename of the output *.flac
