bb8351e074e3153a915af26956182b2c757ecec0
ke260g <ke260g@gmail.com>
2020-10-08 16:11:29 +0800

    ”some string operation on c++"

---
+++ langC++/string/string_common.cpp
@@ -0,0 +1,15 @@
+int isalnum(int c); // isalpha || isdigit
+int islower(int c); // 'a' <= c <= 'z'
+int isupper(int c); // 'A' <= c <= 'Z'
+
+int isalpha(int c); // islower || isupper
+int isdigit(int c); // '0' <= c <= '9'
+
+int iscntrl(int c);
+int isgraph(int c);
+int isprint(int c);
+int ispunct(int c);
+int isspace(int c);
+int isxdigit(int c);
+int isascii(int c);
+int isblank(int c);
