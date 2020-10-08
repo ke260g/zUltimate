6d3ff99edfbfa9e85a7afcbce332050752e60bdd
ke260g <ke260g@gmail.com>
2020-10-08 16:49:56 +0800

    "LeetCode some TwoPtr proble"

--- gAlgorithm/LeetCode2020/TwoPtr/00344. Reverse String.cpp
+++ gAlgorithm/LeetCode2020/TwoPtr/00344. Reverse String.cpp
@@ -5,7 +5,7 @@
 // Output: ["h","a","n","n","a","H"]
 
 // 翻转字符串
-class Solution {
+class Solution { // 索引标记法
 public:
     void reverseString(vector<char>& s) {
         if (s.empty()) return;
@@ -13,5 +13,15 @@ public:
         for (int i = 0; i < mid; ++i) {
             swap(s[i], s[s.size()-i-1]);
         }
+    }
+};
+
+class Solution { // 双指针前后逼近法
+public:
+    void reverseString(vector<char>& s) {
+        if (s.empty()) return;
+        int head = 0, tail = s.size()-1;
+        while (head < tail)
+            swap(s[head++], s[tail--]);
     }
 };
